7fd2a20f17ffcbda01681f277cafb76b67fc2d91
Yevhen_Semeniuk <jekasemenuk@icloud.com>
2021-10-02 23:14:59 +0300

    server: ADD: API event

---
+++ client/src/API/userAPI.js
@@ -0,0 +1,27 @@
+module.exports = {
+   getAllUsers() {
+      return (dispatch) => {
+
+         return dispatch();
+      }
+   },
+ 
+   getUserByID(user_id) {
+      return (dispatch) => {
+
+         return dispatch();
+      }
+   },
+   updateUserByID(user_id) {
+      return (dispatch) => {
+
+         return dispatch();
+      }
+   },
+   deleteUserByID(user_id) {
+      return (dispatch) => {
+
+         return dispatch();
+      }
+   },
+}
