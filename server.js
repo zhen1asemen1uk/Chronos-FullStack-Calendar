cd6142fcbe2dbe00beb956a815b487a3cfba2545
Igor <sergienko339@gmail.com>
2022-06-30 16:24:54 +0300

    fix CORS_2

--- server.js
+++ server.js
@@ -23,6 +23,17 @@ app.use(express.static(`public`));
 
 app.use(require(`./routers`));
 
+//for CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+    );
+    next();
+});
 app.use((req, res) => {
     res.status(404).send(`Unknown Request`);
 });
